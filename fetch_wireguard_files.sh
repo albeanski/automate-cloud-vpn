da51824d1f805673cf966ee1275ddd765dfafce1
albeanski <albeanski@gmail.com>
2022-07-31 08:49:24 -0400

    Added remote rsync option to fetch_wireguard_files.sh script

--- fetch_wireguard_files.sh
+++ fetch_wireguard_files.sh
@@ -4,6 +4,8 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+container_path="/wireguard/client"
+
 usage() {
  echo "usage: ./fetch_wireguard_files.sh [OPTIONS] [CONTAINER NAME]
 
@@ -12,8 +14,9 @@ CONTAINER_NAME          Specify container name instead of searching in docker-co
 
 OPTIONS
 -------
--p DESTINATION_PATH     Destination path to copy files to"
-
+-p DESTINATION_PATH     Destination path to copy files to
+-r REMOTE_HOST          Use rsync to copy files to remote host (ie: 192.168.0.101 or my-other-host.mydomain.com)
+"
 }
 
 exit_usage() {
@@ -21,11 +24,52 @@ exit_usage() {
   exit 1
 }
 
+# $1 container name
+# $2 container path
+# $3 local path
+fetch_local() {
+  echo "docker exec ${1} cat ${2}/privatekey | sudo tee ${3}/privatekey"
+  docker exec ${1} cat ${2}/privatekey | sudo tee "${3}/privatekey" > /dev/null
+
+  echo "docker exec ${1} cat ${2}/publickey | sudo tee ${3}/publickey"
+  docker exec ${1} cat ${2}/publickey | sudo tee "${3}/publickey" > /dev/null
+
+  echo "docker exec ${1} cat ${2}/wg0.conf | sudo tee ${3}/wg0.conf"
+  docker exec ${1} cat ${2}/wg0.conf | sudo tee "${3}/wg0.conf" > /dev/null
+}
+
+# $1 container name
+# $2 container path
+# $3 user@host:dest_path
+fetch_remote() {
+  timestamp=$(date +%N)
+  tmp_path="/tmp/wg_fetch_${timestamp}"
+  mkdir "${tmp_path}"
+  echo "docker exec ${1} cat ${2}/privatekey | sudo tee ${tmp_path}/privatekey"
+  docker exec ${1} cat ${2}/privatekey | sudo tee "${tmp_path}/privatekey" > /dev/null
+
+  echo "docker exec ${1} cat ${2}/publickey | sudo tee ${tmp_path}/publickey"
+  docker exec ${1} cat ${2}/publickey | sudo tee "${tmp_path}/publickey" > /dev/null
+
+  echo "docker exec ${1} cat ${2}/wg0.conf | sudo tee ${tmp_path}/wg0.conf"
+  docker exec ${1} cat ${2}/wg0.conf | sudo tee "${tmp_path}/wg0.conf" > /dev/null
+
+  rsync -v "${tmp_path}/privatekey" "${tmp_path}/publickey" "${tmp_path}/wg0.conf" "${3}"
+
+  rm -rf "${tmp_path}"
+}
+
 path="/etc/wireguard"
-while getopts "p::" options; do
+
+while getopts "p:r::" options; do
 case "${options}" in
   p)
-    path="${OPTARG}"
+    path_arg="${OPTARG}"
+    path="${path_arg}"
+    ;;
+
+  r)
+    remote="${OPTARG}"
     ;;
 
   :)
@@ -42,6 +86,7 @@ done
 # Remove any trailing slashes
 path="${path%"${path##*[!/]}"}"
 
+
 # Ensure only one non-opt argument at the end
 shift $((OPTIND-1))
 if [ "$#" -gt 1 ]; then
@@ -51,22 +96,33 @@ fi
 # Get container name from docker-compose.yml if it wasn't provided
 if [ -z "${1}" ]; then
   container_name=$(cat ./docker-compose.yml | grep -oP -e "(?<=container_name:).*$" )
-else  container_name="${1}"
+else
+  container_name="${1}"
 fi
 
-# Ensure path exists
-if [ ! -z "${path}" ] && [ ! -d "${path}" ]; then
-  echo "Directory doesnt exist: ${path}"
-  exit 1
+if [ ! -z "${remote}" ]; then
+  if [ ! -z "${path_arg}" ]; then
+    echo "-r and -p cannot be set at the same time. Choose either -r (remote) or -p (local) only."
+    exit 1
+  fi
+
+  # remote dest was specified
+  if ! which rsync; then
+    # Ensure rsync is installed if using -r
+    echo "Using -r option requires rsync fo be installed on this machine."
+    exit 1
+  fi
+
+  fetch_remote "${container_name}" "${container_path}" "${remote}"
+
+else
+  # Ensure path exists
+  if [ ! -z "${path}" ] && [ ! -d "${path}" ]; then
+    echo "Directory doesnt exist: ${path}"
+    exit 1
+  fi
+
+  fetch_local "${container_name}" "${container_path}" "${path}"
+
 fi
 
-container_path="/wireguard/client"
-
-echo "docker exec ${container_name} cat ${container_path}/privatekey | sudo tee ${path}/privatekey"
-docker exec ${container_name} cat ${container_path}/privatekey | sudo tee "${path}/privatekey" > /dev/null
-
-echo "docker exec ${container_name} cat ${container_path}/publickey | sudo tee ${path}/publickey"
-docker exec ${container_name} cat ${container_path}/publickey | sudo tee "${path}/publickey" > /dev/null
-
-echo "docker exec ${container_name} cat ${container_path}/wg0.conf | sudo tee ${path}/wg0.conf"
-docker exec ${container_name} cat ${container_path}/wg0.conf | sudo tee "${path}/wg0.conf" > /dev/null
