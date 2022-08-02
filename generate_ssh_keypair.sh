34965ff54c4eaaae688408bedfa35d080571e007
albeanski <albeanski@gmail.com>
2022-08-01 20:21:38 -0400

    Moved ssh_keys out of project. Added ssh key gen role as an option for ssh keys Resolves #6

--- generate_ssh_keypair.sh
+++ generate_ssh_keypair.sh
@@ -1,22 +1,20 @@
 #/bin/sh
-if [ -d "./project" ]; then
-  key_directory="${PWD}/project/files"
-else
-  key_directory="${PWD}/files"
-fi
+key_directory="${PWD}/ssh_keys"
 
 private_key="${key_directory}/ssh_key"
 public_key="${private_key}.pub"
 
 if [ ! -d "${key_directory}" ]; then
-  "Creating '${key_directory}' directory..."
+  echo "'${key_directory}' directory not found..."
+  echo "mkdir ${key_directory}"
   mkdir "${key_directory}"
 fi
 
 # Generate an ssh keypair with no password and place in files directory
-if ssh-keygen -f "${private_key}" -P ""; then
-  cat "${PWD}/files/ssh_key.pub"
-
+echo "ssh-keygen -f ${private_key} -P \"\" -q"
+if ssh-keygen -f "${private_key}" -P "" -q; then
   # Ensure restrictive permissions
+  echo "chmod 600 ${public_key}"
+  echo "chmod 600 ${private_key}"
   chmod 600 "${public_key}" "${private_key}"
 fi
